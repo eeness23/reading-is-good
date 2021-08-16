39c559a7e8dfc87ca26aa9673ee0a7de88556f21
Enes Demirbas <enesdemirbas95@gmail.com>
2021-08-16 13:05:32 +0300

    added i18n for exception messages

---
+++ src/main/java/com/enes/readingisgood/configuration/i18nConfiguration.java
@@ -0,0 +1,36 @@
+package com.enes.readingisgood.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+@Configuration
+public class i18nConfiguration {
+    @Value("${i18n.base-path}")
+    private String basePath;
+
+    @Value("${i18n.default-locale}")
+    private String defaultLocale;
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename(basePath);
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver resolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale(defaultLocale));
+        return localeResolver;
+    }
+}
