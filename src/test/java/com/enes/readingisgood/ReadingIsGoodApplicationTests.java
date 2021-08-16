59d23d96e2ade915fa01d1aaef7e97fac742be8c
Enes Demirbas <enesdemirbas95@gmail.com>
2021-08-16 13:00:13 +0300

    initial commit

---
+++ src/test/java/com/enes/readingisgood/ReadingIsGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enes.readingisgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingIsGoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
